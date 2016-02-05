ae6f7c54647738a895853ff7d2c8f39a864d912c
M L <MCL@MCL-Mac.local>
2016-02-05 16:13:02 -0500

    Made working version with some new messages and new connections between the rooms and going between the rooms works and it starts you over and everything; it's really nice

--- Adventure!.c
+++ Adventure!.c
@@ -4,11 +4,11 @@ int main() {
     enum {NORTH, EAST, SOUTH, WEST};
     typedef struct room {
         char *message;
-        struct room *exits;
+        struct room *exits[4];
     } room;
     char *sign = "Dare you enter the Maze of Doom?";
     char *giveup = "You shrug your shoulders, heave a sigh, and turn around "
-                   "and walk back home.\nIt probably wouldn't have been very "
+                   "and walk back home.\n\nIt probably wouldn't have been very "
                    "fun anyway.";
     char *cavein = "With a sickening crunch, the door slides down behind you.\n"
                    "You are trapped.";
@@ -16,38 +16,77 @@ int main() {
                      "get out of the deal is a chipped fingernail.";
     char *exitsign = "A faint glow alerts you that the end is near at hand.";
     char *falsealarm = "Curses! It was only a cave sprite.";
-    char *escape = "You blink the light from your eyes. How many centuries has "
-                   "it been since you've seen the sun?\nTo your south there is "
-                   "a sign, reassuring in its solid, manmade construction. "
-                   "You read it:";
+    char *escape = "You blink the light from your eyes.\nHow many centuries "
+                   "has it been since you've seen the sun?\n\nTo your south "
+                   "there is a sign, reassuring in its solid, manmade "
+                   "construction.\nYou turn to read it: ";
+    char *angus = "You hear the faint skirl of a bagpipe.";
+    char *stalactite = "A drop of water from a stalactite brushes your cheek. "
+                       "It stings a little.";
     char *normal = "Your footsteps resound from the cave walls.";
     room *rooms[6];
     room *exit = &(room) {escape, {0, 0, 0, 0} };
-    rooms[0] = &(room) {cavein, {rooms[5], rooms[1], 0, rooms[2]} };
-    rooms[1] = &(room) {normal, {rooms[3], 0, 0, rooms[0]} };
-    rooms[2] = &(room) {normal, {0, rooms[0], rooms[4], 0} };
-    rooms[3] = &(room) {normal, {exit, rooms[5], rooms[1], 0} };
-    rooms[4] = &(room) {normal, {rooms[2], 0, 0, 0} };
-    rooms[5] = &(room) {normal, {0, 0, rooms[0], rooms[3]} };
+    // create initial rooms
+    rooms[0] = &(room) {normal, {0, 0, 0, 0} };
+    rooms[1] = &(room) {normal, {0, 0, 0, 0} };
+    rooms[2] = &(room) {normal, {0, 0, 0, 0} };
+    rooms[3] = &(room) {exitsign, {0, 0, 0, 0} };
+    rooms[4] = &(room) {normal, {0, 0, 0, 0} };
+    rooms[5] = &(room) {falsealarm, {0, 0, 0, 0} };
+    // redefine rooms to connect to eachother
+    *rooms[0] = (room) {stalactite, {rooms[4], rooms[1], 0, rooms[2]} };
+    *rooms[1] = (room) {normal, {rooms[3], rooms[4], 0, rooms[0]} };
+    *rooms[2] = (room) {angus, {0, rooms[0], rooms[4], 0} };
+    *rooms[3] = (room) {exitsign, {exit, rooms[5], rooms[1], 0} };
+    *rooms[4] = (room) {normal, {rooms[2], 0, rooms[0], rooms[1]} };
+    *rooms[5] = (room) {falsealarm, {0, 0, 0, rooms[3]} };
 
     printf("%s ", sign);
     char response = 0;
     scanf(" %c", &response); // get the first non-whitespace character
     while (1) {
         if (response == 'Y' || response == 'y') {
-            printf("Oh, hey, that was really fun!\n");
-            break;
-            // enter
+            room *here = rooms[0];
+            room *there = 0;
+            printf("%s\n", cavein);
+            while (here != exit) {
+                printf("> ");
+                scanf("%*[^\n]"); // consume the rest of the previous line
+                scanf(" %c", &response);
+                switch(response) {
+                    case 'N':
+                    case 'n':
+                        there = here->exits[NORTH];
+                        break;
+                    case 'S':
+                    case 's':
+                        there = here->exits[SOUTH];
+                        break;
+                    case 'E':
+                    case 'e':
+                        there = here->exits[EAST];
+                        break;
+                    case 'W':
+                    case 'w':
+                        there = here->exits[WEST];
+                        break;
+                }
+                if (there == 0) {
+                    printf("%s\n", wrongway);
+                }
+                else {
+                    here = there;
+                    printf("%s\n", here->message);
+                }
+            }
         }
         else if (response == 'N' || response == 'n') {
             printf("%s\n", giveup);
             return 0;
         }
-        else {
-            // the user gave an invalid response, so ask again
-            printf("%s ", sign);
-            scanf("%*[^\n]"); // consume the rest of the line
-            scanf(" %c", &response);
-        }
+        // the user gave an invalid response or escaped, so ask again
+        printf("%s ", sign);
+        scanf("%*[^\n]");
+        scanf(" %c", &response);
     }
 }
