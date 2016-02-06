ed28cb8b3beb269ec9c4c8a48d66350f7c35c0db
M L <MCL@MCL-Mac.local>
2016-02-06 02:28:32 -0500

    Added IKEA elf and changed the prompt to ask questions to fit the assignment; also, assumed that any answer to "Do you want to play?" other than ones starting with n is a yes

--- Adventure!.c
+++ Adventure!.c
@@ -12,6 +12,18 @@ int main() {
                    "fun anyway.";
     char *cavein = "With a sickening crunch, the door slides down behind you.\n"
                    "You are trapped.";
+    char *ikea = "With a flash and a shower of surprisingly sturdy blue bags, "
+                 "a mystical IKEA elf stands before you.\n\"Pardon me, friend, "
+                 "but I couldn't help noticing that you seemed a little lost. "
+                 "Would you mind terribly if I showed you to wherever you are "
+                 "going?\"";
+    char *nohelp = "\"I am sorry to have been a bother.\" It disappears, and a "
+                   "set of tasteful matched silverware clatters down into the "
+                   "shadows.";
+    char *shortcut = "The IKEA elf takes you by the hand and whirls you "
+                     "through a tangle of vaguely flower-shaped light fixtures "
+                     "and bumpy chocolate-filled cookies, and then disappears "
+                     "in a final dazzle of cotton sheets and laptop stands.";
     char *wrongway = "You scrabble desperately at the cave wall, but all you "
                      "get out of the deal is a chipped fingernail.";
     char *exitsign = "A faint glow alerts you that the end is near at hand.";
@@ -22,7 +34,9 @@ int main() {
                    "construction.\nYou turn to read it: ";
     char *angus = "You hear the faint skirl of a bagpipe.";
     char *stalactite = "A drop of water from a stalactite brushes your cheek. "
-                       "It stings a little.";
+                       "It stings a little.\n\n"
+                       "A red tasselled pull cord hangs unobtrusively off to "
+                       "one side.";
     char *normal = "Your footsteps resound from the cave walls.";
     room *rooms[6];
     room *exit = &(room) {escape, {0, 0, 0, 0} };
@@ -50,7 +64,7 @@ int main() {
             room *there = 0;
             printf("%s\n", cavein);
             while (here != exit) {
-                printf("> ");
+                printf("Which direction do you try? [n,s,e,w] ");
                 scanf("%*[^\n]"); // consume the rest of the previous line
                 scanf(" %c", &response);
                 switch(response) {
@@ -70,6 +84,30 @@ int main() {
                     case 'w':
                         there = here->exits[WEST];
                         break;
+                    case 'P':
+                    case 'p':
+                        if (here == rooms[0]) {
+                            printf("You pull the cord.\n");
+                        }
+                        else {
+                            break;
+                        }
+                    case 'H':
+                    case 'h':
+                        printf("%s ", ikea);
+                        scanf("%*[^\n]");
+                        scanf("%*1[\n]");
+                        scanf("%*[ \t]");
+                        scanf("%c", &response);
+                        if (response == 'N' || response == 'n') {
+                            printf("%s\n", shortcut);
+                            there = exit;
+                        }
+                        else {
+                            printf("%s\n", nohelp);
+                            there = here;
+                        }
+                        break;
                 }
                 if (there == 0) {
                     printf("%s\n", wrongway);
@@ -84,7 +122,12 @@ int main() {
             printf("%s\n", giveup);
             return 0;
         }
-        // the user gave an invalid response or escaped, so ask again
+        else {
+            // Come on, who _doesn't_ want to enter the Maze of Doom?
+            response = 'y';
+            continue;
+        }
+        // the user escaped, but there's that sign again in nice bold letters
         printf("%s ", sign);
         scanf("%*[^\n]");
         scanf(" %c", &response);
